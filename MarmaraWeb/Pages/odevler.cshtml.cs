03b98959f930588978413c7e942d8eab2ff1e4d6
YusufHayri <yusuf.hayri@marun.edu.tr>
2021-05-29 22:18:06 +0300

    resimler, linkler ve js düzeltildi

---
+++ MarmaraWeb/Pages/odevler.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MarmaraWeb.Pages
+{
+    public class odevlerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- MarmaraWeb/wwwroot/js/site.js
+++ MarmaraWeb/wwwroot/js/site.js
@@ -1,4 +1,27 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function dmOnClick() {
+    const button = document.querySelector('.dark-mode-button');
+    const nav = document.querySelector('nav');
+    const header = document.querySelector('header');
+    const body = document.querySelector('body');
 
-// Write your JavaScript code.
+    if (button.classList.contains('bg-light')) {
+        button.classList.remove('bg-light', 'btn-ligt');
+        button.classList.add('bg-dark', 'btn-dark');
+        nav.classList.add('bg-light', 'navbar-ligt');
+        nav.classList.remove('bg-dark', 'navbar-dark');
+        header.classList.add('bg-light', 'navbar-ligt');
+        header.classList.remove('bg-dark', 'navbar-dark');
+        body.style.backgroundColor = "#f8f9fa";
+    }
+    else {
+        button.classList.add('bg-light', 'btn-ligt');
+        button.classList.remove('bg-dark', 'btn-dark');
+        nav.classList.remove('bg-light', 'navbar-ligt');
+        nav.classList.add('bg-dark', 'navbar-dark');
+        header.classList.remove('bg-light', 'navbar-ligt');
+        header.classList.add('bg-dark', 'navbar-dark');
+        body.style.backgroundColor = "#343a40";
+    }
+}
+const button = document.querySelector('.dark-mode-button');
+button.addEventListener('click', dmOnClick);
