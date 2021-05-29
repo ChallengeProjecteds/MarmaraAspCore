41e6b1d4bed380943730b945e99ac510b810da73
Haktan Kocyigit <haktankocyigit@gmail.com>
2021-05-29 23:30:10 +0300

    odevler sayfasına projeler json'u eklendi.

--- MarmaraWeb/Pages/odevler.cshtml.cs
+++ MarmaraWeb/Pages/odevler.cshtml.cs
@@ -2,15 +2,29 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MarmaraWeb.Models;
+using MarmaraWeb.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace MarmaraWeb.Pages
 {
     public class odevlerModel : PageModel
     {
+        public JsonProjectService JsonProjectService;
+        public IEnumerable<ProjectModel> Projects;
+
+        private readonly ILogger<odevlerModel> _logger;
+        public odevlerModel(ILogger<odevlerModel> logger, JsonProjectService jsonprojectservice)
+        {
+            _logger = logger;
+            JsonProjectService = jsonprojectservice;
+        }
+
         public void OnGet()
         {
+            Projects = JsonProjectService.GetProjects();
         }
     }
 }
